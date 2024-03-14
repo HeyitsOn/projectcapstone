e5651ecb6ee6f70b912876ad341e23ca9231e8cc
onika <silekuonika02gmail.com>
2024-03-14 16:25:37 +0200

    contact

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,30 +1,37 @@
 <template>
   <div class="contact">
     <div class="contact-section">
-      <h1> Get IN TOUCH WITH US </h1>
+      <h1>Get IN TOUCH WITH US</h1>
     </div>        
     <div class="row">
-        <div class="col-4 contact-header">
-        <div class=" justify-content-center">
-          <div class=" about-content">
-            <p class="landline"><img src="https://i.ibb.co/McGLXQY/landline.png" class="small-image"> 021 245 8874</p>
-            <p class="Email"><img src="https://i.ibb.co/FKqgRhN/arroba.png" class="small-image"> infor@Snowlakebooking</p>
-            <p class="web"><img src="https://i.ibb.co/sK6LpK7/broswer.png" class="small-image">www.Snowlake.com</p>
+      <div class="col-4 contact-header">
+        <div class="justify-content-center">
+          <div class="about-content">
+            <p class="contact-info"><img src="https://i.ibb.co/McGLXQY/landline.png" class="small-image">021 245 8874</p>
+            <p class="contact-info"><img src="https://i.ibb.co/FKqgRhN/arroba.png" class="small-image">info@Snowlakebooking</p>
+            <p class="contact-info"><img src="https://i.ibb.co/sK6LpK7/broswer.png" class="small-image">www.Snowlake.com</p>
           </div>
         </div>
-        </div>
-        <div class="col-5 contact-body">
-          <div class="justify-content-center">
-            <div class="">
-              <img src="https://www.essence.com/wp-content/uploads/2019/06/IMG_1833.jpg" width="400" alt="">
-            </div>
+      </div>
+      <div class="col-5 contact-body">
+        <div class="justify-content-center">
+          <div>
+            <img src="https://www.essence.com/wp-content/uploads/2019/06/IMG_1833.jpg" width="400" alt="">
           </div>
         </div>
+      </div>
     </div>
   </div>
-    
-  </template>
-  <style>
+</template>
+
+<style>
 .small-image {
   max-width: 50px; /* Adjust the size as needed */
-}</style>
+}
+
+.contact-info {
+  display: flex;
+  align-items: center;
+  margin-left: auto;
+}
+</style>
